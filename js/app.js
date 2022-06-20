96d2ad2fce1eb1fa78388c7c8fe3b6d3f85693ee
Hendervi Hunda <hendervihunda16@gmail.com>
2022-06-20 10:09:13 -0400

    primer-commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+
+menu.addEventListener('click', () =>{
+    nav.classList.toggle('active');
+    console.log(menu);
+});
